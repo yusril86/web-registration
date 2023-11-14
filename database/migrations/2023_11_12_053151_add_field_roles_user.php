6aba77d31873a8119aec676469831840759cb31b
Yusril Anwar <yusril886@gmail.com>
2023-11-14 13:34:38 +0800

    update migration

--- database/migrations/2023_11_12_053151_add_field_roles_user.php
+++ database/migrations/2023_11_12_053151_add_field_roles_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('roles')->default('ADMIN');
+            $table->string('roles')->default('Admin');
         });
     }
 
